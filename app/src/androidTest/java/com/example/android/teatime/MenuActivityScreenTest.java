80b2377ca02d7db798cc6049dedb292506ba572d
The Aviator <abhi@xyz>
2018-01-28 05:44:15 +0530

    Initial Commit with OrderActivity Test implemented

---
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -0,0 +1,8 @@
+package com.example.android.teatime;
+
+/**
+ * Created by aviator on 28/01/18.
+ */
+
+public class MenuActivityScreenTest {
+}
---
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -0,0 +1,51 @@
+package com.example.android.teatime;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by aviator on 28/01/18.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class OrderActivityBasicTest {
+    @Rule
+    public ActivityTestRule<OrderActivity> activityActivityTestRule =
+            new ActivityTestRule<OrderActivity>(OrderActivity.class);
+
+    @Test
+    public void clickIncrementButton_ChangesQuantityAndCost() {
+        //1. Find the view
+        //2. Perform the action on the view
+        onView(withId(R.id.increment_button)).perform(click());
+
+        //3. Check if the view does what you expected
+        onView(withId(R.id.quantity_text_view)).check(matches(withText("1")));
+        onView(withId(R.id.cost_text_view)).check(matches(withText("₹ 5.00")));
+    }
+
+    @Test
+    public void clickDecrementButton_ChangesQuantityAndCost() {
+
+        // Check the initial quantity variable is zero
+        onView((withId(R.id.quantity_text_view))).check(matches(withText("0")));
+
+        //1. Find the view
+        //2. Perform the action on the view
+        onView(withId(R.id.decrement_button)).perform(click());
+
+        //3. Check if the view does what you expected
+        onView(withId(R.id.quantity_text_view)).check(matches(withText("0")));
+        onView(withId(R.id.cost_text_view)).check(matches(withText("$0.00")));
+    }
+}
